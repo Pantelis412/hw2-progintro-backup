d2d7dc2f8e5bab46f5b3e947fdabf4c152df4106
Pantelis <sdi2300079@di.uoa.gr>
2023-12-27 21:03:52 +0200

    minor updates to the code

--- coop/src/coop.c
+++ coop/src/coop.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
-int InputFirstOne(void){
+int InputFirstOne(void){/*getchar will only accept one character for the main programm*/
     int ch=getchar();
-    while(ch!=EOF && ch!='C' && ch!='D'){
+    while(ch!=EOF && ch!='C' && ch!='D'){/*make sure that the only acceptable value is one of these three*/
        ch = getchar(); // Update the value of ch to read the next character 
     }
     return ch;
 }
 int main(void){
     int counter =0, cha;
-    while(counter<10){/*idea:   if ch=getchar()==EOF break;*/
+    while(counter<1e+6){
         if(counter % 1000 !=999){
             putchar('D');
             fflush(stdout);
             putchar('\n');
             fflush(stdout);
-            printf("%d\n",counter);
         }
         else{
             putchar('C');
             fflush(stdout);
             putchar('\n');
             fflush(stdout);
-            printf("EWQ%d\n",counter);
         }
         cha=InputFirstOne();
         if (cha==EOF) break;
