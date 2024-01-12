55bfb7b584358c11d12eeb565bc033b558062d3c
Pantelis <sdi2300079@di.uoa.gr>
2024-01-12 23:15:21 +0200

    comments completed

--- fauxtoshop/src/fauxtoshop.c
+++ fauxtoshop/src/fauxtoshop.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+// #include <string.h>
 #define MIN_HEADER_SIZE 54
 #define WIDTH 18
 #define HEIGHT 22
@@ -9,13 +9,14 @@
 #define FILE_SIZE 2
 #define IMAGE_SIZE 34
 
+//We use these global variables to use some data we read from the header in the proccesing of the pixel array
 int offset;
 int o_padding;
 int n_padding;
 int o_width;
 int o_height;
 
-void change_header() {
+void change_header() {//this function is used to check that the input is a valid bmp file and to make the necessary changes for the 90 degreed rotation
   char header[MIN_HEADER_SIZE];
   int read = fread(header, MIN_HEADER_SIZE, 1, stdin); //we use fread to read the header of a bmp file
   // we check if fread has read the proper number of bytes from a proper bmp file
@@ -30,24 +31,26 @@ void change_header() {
   }
   // we check if the bmp file uses 24 bits per pixel
   int bits;
-  bits = header[BITS_PER_PIXEL] + header[BITS_PER_PIXEL + 1] * 256;
+  bits = header[BITS_PER_PIXEL] + header[BITS_PER_PIXEL + 1] * 256;//the number of bits for each pixel is stored in 2 bytes in little endian so we use this technique to read them
   if (bits != 24) {
     fprintf(stderr, "Wrong bits per pixel size");
     exit(1);
   }
-  int width, height; /*we save width and height of the bmp file in these integer
-                        type variables*/
-  width = *(unsigned int *)&header[WIDTH];
+  //We change the information about width and height of the image stored in the header
+  int width, height; //we save width and height of the bmp file in these integer
+                     //type variables
+  width = *(unsigned int *)&header[WIDTH];//we read the address where the first byte of width is store and with casting we take the 4 bytes we need
   o_width = width;
-  height = *(unsigned int *)(header + HEIGHT);
+  height = *(unsigned int *)(header + HEIGHT);//we do the same thing for heigth. (header + HEIGHT) is equal to &header[HEIGTH] because <header> is like a pointer 
   o_height = height;
   // We swap width and height in the header
   *(unsigned int *)&header[WIDTH] = height;
   *(unsigned int *)&header[HEIGHT] = width;
+  //we use the same technique to store the offset
   offset = *(unsigned int *)&header[OFFSET];
   //we calculate the new and the old padding
-  // based on the width and heigth of the header and we change the information
-  // about he file size and the image size of the header
+  // based on the width and heigth of the header. 
+  // Then, we change the information about the file size and the image size of the header
   int old_padding;
   if (((3 * width) % 4) == 0)
     old_padding = 0;
@@ -71,11 +74,9 @@ void change_header() {
 void change_otherdata() {
   int extra = offset - 54;
   // if there are no "other data" the function does nothing
-  if (extra == 0) {
-  }
   // else we dynamicly create an array in which we read the othere data(or the
   // extra data of an extended header or both) and write it in our file
-  else {
+  if (extra != 0) {
     char *other;
     other = (char *)malloc(extra);
     int read = fread(other, sizeof(char), extra, stdin);
@@ -143,18 +144,20 @@ void pixelarray() {
   }
   free(array1);
 
-  //Step 3, we want to rotate the pixel array so that the image we make has rotated by 90 degrees right.
+  //Step 3, we want to rotate the pixel array so that the image we make has been rotated by 90 degrees to the right.
   //However, the pixel array is inverted so the first line we read actually depicts the bottom line of the picture.
-  //Moreover, ervery pixel conists of three bytes,which means three consecutive elements of the array.
-  //Thus, we want to 
+  //Moreover, every pixel conists of three bytes,which means three consecutive elements of the array.
+  //Thus, we want to read array2 from right to left, taking 3 elements at a time 
+  //After each line, we add the padding if necessary
   char p = 'P';
   for (int j = 3 * o_width - 1; j >= 0; j -= 3) {
     for (int i = 0; i < o_height; i++) {  // it writes three bytes every time to
                                           // correclty represent the pixels
-      fwrite(&array2[i][j - 2], sizeof(char), 1, stdout);
-      fwrite(&array2[i][j - 1], sizeof(char), 1, stdout);
+      fwrite(&array2[i][j - 2], sizeof(char), 1, stdout);//We push the elements we read immediately to stdout
+      fwrite(&array2[i][j - 1], sizeof(char), 1, stdout);//so we don't need to use another array to store them.
       fwrite(&array2[i][j], sizeof(char), 1, stdout);
     }
+    //after each new line we add the necessary padding
     for (int k = 1; k <= n_padding; k++) {
       fwrite(&p, sizeof(char), 1, stdout);
     }
