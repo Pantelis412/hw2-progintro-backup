8673e9e9cbcff381cadbee3d6614d714f17ab1b2
Pantelis <sdi2300079@di.uoa.gr>
2024-01-13 20:31:09 +0200

    comments and readme coop

--- coop/src/coop.c
+++ coop/src/coop.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-int InputFirstOne(void){/*getchar will only accept one character for the main programm*/
+int InputFirstOne(void){
     int ch=getchar();
     while(ch!=EOF && ch!='C' && ch!='D'){/*make sure that the only acceptable value is one of these three*/
-       ch = getchar(); // Update the value of ch to read the next character 
+       ch = getchar(); // Update the value of ch to read the next character if the one previously read was not among these 3
     }
     return ch;
 }
 int main(void){
     int counter=0, cha;
-    while(counter<1e+6){/*we have this condition to make sure that*/
-        if(counter % 1000 !=999){
+    while(counter<1e+6){/*we have this condition to make sure that the max rounds to be played are 10^6*/
+        if(counter % 1000 !=999){//every 1000 rounds we must play a 'C'
             putchar('D');
             fflush(stdout);
             putchar('\n');
@@ -21,7 +21,7 @@ int main(void){
             putchar('\n');
             fflush(stdout);
         }
-        cha=InputFirstOne();
+        cha=InputFirstOne();//we use this function to read a valid character from the opponent (stdin)
         if (cha==EOF) break;
         counter++;
     }
